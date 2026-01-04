b1f6b1bfb6447e4c440fd266bc7daebd6adccca5
QbSssProjects <kubek.w1234@gmail.com>
2026-01-04 16:20:08 +0100

    Init

---
+++ src/main/java/qbsss/noCraftingCore/Main.java
@@ -0,0 +1,17 @@
+package qbsss.noCraftingCore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
